69683f00f6107d58f80a8545adfa4bfa0ee39a56
Thverney-Dozo <theo7420@hotmail.fr>
2021-02-08 21:51:52 +0100

    fix

--- iterators/random_access_iterator.hpp
+++ iterators/random_access_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: aeoithd <aeoithd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 13:23:34 by thverney          #+#    #+#             */
-/*   Updated: 2021/02/08 17:34:01 by aeoithd          ###   ########.fr       */
+/*   Updated: 2021/02/08 20:19:38 by aeoithd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,19 @@ namespace ft
 	class random_access_iterator : public bidirectional_iterator<T, B>
 	{
 		public:
-            typedef typename ft::bidirectional_iterator<T, B>::difference_type	difference_type;
+            typedef typename ft::bidirectional_iterator<T, B>::difference_type	    difference_type;
 			typedef typename ft::bidirectional_iterator<T, B>::value_type			value_type;
 			typedef typename ft::bidirectional_iterator<T, B>::size_type			size_type;
 			
 			typedef typename ft::bidirectional_iterator<T, B>::reference			reference;
-			typedef typename ft::bidirectional_iterator<T, B>::pointer			pointer;
+			typedef typename ft::bidirectional_iterator<T, B>::pointer			    pointer;
 			typedef typename ft::bidirectional_iterator<T, B>::non_const_pointer	non_const_pointer;
+        	typedef typename ft::bidirectional_iterator<T, B>						bidirectional_iterator;
+
+        random_iterator(nonConstPointer val = 0) : bidirec_iterator(val) {}
+
+        private:
+            _ptr;
     };
 }
 
