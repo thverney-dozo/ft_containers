c971fcc705609b6c44a882c598e94fd148838409
Thverney-Dozo <theo7420@hotmail.fr>
2021-02-09 22:39:42 +0100

    fix

--- iterators/checkers.hpp
+++ iterators/checkers.hpp
@@ -6,13 +6,21 @@
 /*   By: aeoithd <aeoithd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 22:37:13 by aeoithd           #+#    #+#             */
-/*   Updated: 2021/02/09 22:38:14 by aeoithd          ###   ########.fr       */
+/*   Updated: 2021/02/09 22:39:36 by aeoithd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECKERS_HPP
 # define CHECKERS_HPP
+namespace ft
+{
+    template <bool B>
+    struct enable_if {};
 
-
+    template <>
+    struct enable_if<true> {
+        typedef int type;
+    };
+}
 
 #endif
