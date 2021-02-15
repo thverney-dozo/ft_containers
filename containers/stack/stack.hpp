7dd8249c88e976b1ac6d4430d46f458654732711
Thverney-Dozo <theo7420@hotmail.fr>
2021-02-15 10:00:13 +0100

    fix

--- containers/stack/stack.hpp
+++ containers/stack/stack.hpp
@@ -3,10 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   stack.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thverney <thverney@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aeoithd <aeoithd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 12:47:16 by thverney          #+#    #+#             */
-/*   Updated: 2021/02/05 12:47:17 by thverney         ###   ########.fr       */
+/*   Updated: 2021/02/15 09:54:17 by aeoithd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef STACK_HPP
+# define STACK_HPP
+
+#include <deque>
+
+namespace ft
+{
+	template <class T, class Container = std::deque<T> >
+	class stack
+	{
+
+		public:
+			typedef T			value_type;
+			typedef Container	container_type;
+			typedef size_t		size_type;
+
+		
+		public:
+
+		    // CAPACITY
+            size_type size(void)        const   { return (c.size()); };
+			bool empty() 				const	{ return c.empty(); };
+            value_type& top()					{ return c.back(); }
+			const value_type& top()     const	{ return c.back(); }
+			void push (const value_type& val)	{ c.push_back(val); }
+			void pop()							{ c.pop_back(); }
+
+		/*		 ██████╗ ██████╗ ██████╗ ██╗     ██╗███████╗███╗   ██╗    ███████╗ ██████╗ ██████╗ ███╗   ███╗
+				██╔════╝██╔═══██╗██╔══██╗██║     ██║██╔════╝████╗  ██║    ██╔════╝██╔═══██╗██╔══██╗████╗ ████║
+				██║     ██║   ██║██████╔╝██║     ██║█████╗  ██╔██╗ ██║    █████╗  ██║   ██║██████╔╝██╔████╔██║
+				██║     ██║   ██║██╔═══╝ ██║     ██║██╔══╝  ██║╚██╗██║    ██╔══╝  ██║   ██║██╔══██╗██║╚██╔╝██║
+				╚██████╗╚██████╔╝██║     ███████╗██║███████╗██║ ╚████║    ██║     ╚██████╔╝██║  ██║██║ ╚═╝ ██║
+				 ╚═════╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝╚══════╝╚═╝  ╚═══╝    ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝                                                                  
+        */
+		public:
+			// Default constructor
+			explicit stack(const container_type &cont = container_type())
+			: c(cont)
+			{};
+			// Copy constructor
+			stack(const stack &cpy) { *this = cpy; };
+			// Affectation overload
+			stack &operator=(const stack &affect)
+			{
+				this->c = affect.c;
+				return (*this);
+			};
+			// destructor
+			~stack() {};
+		protected:
+			container_type c;
+        
+	        friend bool operator==(const stack& a, const stack& b)	{ return a.c == b.c; }
+			friend bool operator!=(const stack& a, const stack& b)	{ return a.c != b.c; }
+			friend bool operator<(const stack& a, const stack& b)	{ return a.c < b.c; }
+			friend bool operator<=(const stack& a, const stack& b)	{ return a.c <= b.c; }
+			friend bool operator>(const stack& a, const stack& b)	{ return a.c > b.c; }
+			friend bool operator>=(const stack& a, const stack& b)	{ return a.c >= b.c; }
+	};
+}
+
+#endif
