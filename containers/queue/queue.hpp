18079eaa65263ac0e4e2c5da1b58d10fa5839dd5
Thverney-Dozo <theo7420@hotmail.fr>
2021-02-15 10:09:44 +0100

    fix

--- containers/queue/queue.hpp
+++ containers/queue/queue.hpp
@@ -6,7 +6,7 @@
 /*   By: aeoithd <aeoithd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 12:47:12 by thverney          #+#    #+#             */
-/*   Updated: 2021/02/15 10:09:25 by aeoithd          ###   ########.fr       */
+/*   Updated: 2021/02/15 10:09:42 by aeoithd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,7 @@
 namespace ft
 {
     template <class T, class Container = std::deque<T> >
+
+};
+
+#endif
