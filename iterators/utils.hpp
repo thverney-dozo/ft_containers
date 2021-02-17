adf47cc168dd26717dbefb8ad79a8ad20699ba8f
Thverney-Dozo <theo7420@hotmail.fr>
2021-02-17 03:45:58 +0100

    fix

--- iterators/utils.hpp
+++ iterators/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: aeoithd <aeoithd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 06:58:47 by aeoithd           #+#    #+#             */
-/*   Updated: 2021/02/17 00:20:05 by aeoithd          ###   ########.fr       */
+/*   Updated: 2021/02/17 03:32:25 by aeoithd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,13 @@
 
 namespace ft
 {
-    template <class Key, class T>
-        struct Node
-        {
-            std::pair<Key, T> pair;
-            Node *left;
-            Node *right;
-            Node *parent;
-            bool end;
-        };
+    template <class T>
+	struct Node
+	{
+		T info;
+		Node *prev;
+		Node *next;
+	};
 }
 
 #endif
